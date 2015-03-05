ec966a8fbdfe432316132fbb8c1c8b3f66bd848e
wudih146 <wudih146126@163.com>
2015-03-05 22:19:06 +0800

    Project
    An empty android project that you can import your code here.

---
+++ src/com/example/softwaremanager/MainView.java
@@ -0,0 +1,15 @@
+package com.example.softwaremanager;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainView extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.mainview);
+	}
+}
